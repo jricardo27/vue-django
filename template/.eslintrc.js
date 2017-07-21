d0c68db4d640742c66c217b29b090540a2cbbbf6
Ricardo Perez <jricardo27@gmail.com>
2017-07-22 09:38:49 +1000

    Improve ESLint configuration

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,16 +1,46 @@
 module.exports = {
-  root: true,
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    root: true,
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'script',
+        ecmaFeatures: {
+            'impliedStrict': true,
+            'jsx': false
+        }
+    },
+    plugins: [
+        'import',
+        'html',  // Required to lint *.vue files.
+        'promise'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:promise/recommended',
+        'plugin:vue/recommended'
+    ],
+    env: {
+        es6: true,
+        jquery: true,
+        browser: true
+    },
+    rules: {
+        // Require parens in arrow function arguments.
+        'arrow-parens': ['error', 'as-needed'],
+        // Enforce consistent indentation.
+        'indent': ['error', 4, {SwitchCase: 1, MemberExpression: 1}],
+        // Require spacing around infix operators.
+        'space-infix-ops': 'error',
+        // Enforce spacing before and after semicolons.
+        'semi-spacing': ['error', {before: false, after: true}],
+
+        // Vue.js
+        // Require v-bind:key with v-for directives.
+        'require-v-for-key': 'on',
+        // Enforce v-bind directive style.
+        'v-bind-style': ['error', 'shorthand'],
+        // Enforce v-on directive style.
+        'v-on-style': ['error', 'shorthand']
+    }
+};
