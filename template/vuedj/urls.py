e5889eaeb2cb965820d338c564507bd6efea23f3
Ricardo Perez <jricardo27@gmail.com>
2017-07-22 22:20:44 +1000

    Use class based views

--- template/vuedj/urls.py
+++ template/vuedj/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import index
+from app.views import IndexView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name='home'),
+    url(r'^$', IndexView.as_view(), name='home'),
 ]
